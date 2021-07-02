6fb38ee6f36d77585c51fb03fb103d8c89a135cd
Kassra-sinaei <kasra.sinaei@gmail.com>
2021-07-02 16:41:06 +0430

    recursive Forward kinematics added

--- trajectory_planner/src/Link.cpp
+++ trajectory_planner/src/Link.cpp
@@ -1,13 +1,17 @@
 #include "Link.h"
 
- _Link::_Link(short int ID, string name,short int parentID, short int numChilds, short int* childID, double length){
-    this->ID_= ID;
-    this->name_ = name;
-    this->parentID_ = parentID;
-    childID_ = new short int[numChilds];
-    for (int i = 0; i < numChilds ; ++i)
-        childID_[i] = childID[i];
-    this->length_ = length;
+ _Link::_Link(short int ID, _Link* parent, Vector3d a, Vector3d b, double m, Matrix3d inerta){
+    this->ID_ - ID;
+    this->parent_ = parent;
+    this->a_ = a;
+    this->b_ = b;
+    this->m_ = m;
+    this->I_ = inerta;
+
+    this->q_ = 0.0;
+    this->dq_ = 0.0;
+    this->ddq_ = 0.0;
+
  }
 
  short int _Link::getID(){
@@ -18,14 +22,64 @@ double _Link::q(){
     return this->q_;
 }
 
-void _Link::q(double config){
-    this->q_ = config;
+void _Link::update(double q, double dq, double ddq){
+    this->q_ = q;
+    this->dq_ = dq;
+    this->ddq_ = ddq;
 }
 
-short int* _Link::getChildID(){
-    return this->childID_;
+Vector3d _Link::getPose(){
+    return this->p_;
 }
 
-short int _Link::getParentID(){
-    return this->parentID_;
+Matrix3d _Link::getRot(){
+    return this->R_;
+}
+
+_Link* _Link::getParent(){
+    return this->parent_;
+}
+
+Matrix3d _Link::rodrigues(Vector3d w, double dt){
+    // Helper Function for calculating attitude in Forward Kinematics
+
+    if (w.norm() < numeric_limits<double>::epsilon()){
+        return Matrix3d::Identity(3,3);
+    }
+    else{
+        Vector3d wn = w/w.norm();
+        double th = w.norm() * dt;
+        Matrix3d w_wedge;
+        w_wedge << 0.0, -wn(2), wn(1),
+                   wn(2), 0.0, -wn(0),
+                   -wn(1), wn(0), 0.0;
+        Matrix3d R = Matrix3d::Identity(3,3) + w_wedge * sin(th) + w_wedge * w_wedge * (1 - cos(th));
+        return R;
+    }
+}
+
+MatrixXd _Link::FK(){
+    if(this->ID_ == 0){
+        return this->transformation();
+    }
+    else{
+        this->parent_->FK();
+        this->p_ = this->parent_->R_ * this->b_ + this->parent_->p_;
+        this->R_ = this->parent_->R_ * this->rodrigues(this->a_, this->q_);
+        return this->transformation();
+    }
+}
+
+MatrixXd _Link::transformation(){
+    // returns homogeneous transformation matrix
+    MatrixXd T(4,4);
+    T << this->R_(0,0), this->R_(0,1), this->R_(0,2), this->p_(0),
+         this->R_(1,0), this->R_(1,1), this->R_(1,2), this->p_(1),
+         this->R_(2,0), this->R_(2,1), this->R_(2,2), this->p_(2),
+         0.0, 0.0, 0.0, 1.0;
+    return T;
+}
+
+_Link::~_Link(){
+
 }
