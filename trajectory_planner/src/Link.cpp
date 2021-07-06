d4075488de650180bdcf5c1ae4b5ec8a291eb7cd
Kassra-sinaei <kasra.sinaei@gmail.com>
2021-07-06 18:27:10 +0430

    Jacobian and Typo Fixed

--- trajectory_planner/src/Link.cpp
+++ trajectory_planner/src/Link.cpp
@@ -1,7 +1,7 @@
-#include "Link.h"
+#include "../include/trajectory_planner/Link.h"
 
-_Link::_Link(short int ID, _Link* parent, Vector3d a, Vector3d b, double m, Matrix3d inertia){
-    this->ID_ - ID;
+_Link::_Link(short int ID, Vector3d a, Vector3d b, double m, Matrix3d inertia, _Link* parent){
+    this->ID_ = ID;
     this->parent_ = parent;
     this->a_ = a;
     this->b_ = b;
@@ -38,6 +38,11 @@ Matrix3d _Link::getRot(){
 
 _Link* _Link::getParent(){
     return this->parent_;
+}
+
+void _Link::initPose(Vector3d p, Matrix3d r){
+	this->p_ = p;
+	this->R_ = r;
 }
 
 Matrix3d _Link::rodrigues(Vector3d w, double dt){
@@ -83,14 +88,19 @@ MatrixXd _Link::transformation(){
 MatrixXd _Link::updateJacobian(){
     vector<_Link> idx;
     idx.push_back(*this);
-    _Link base = *(this->getParent());
-    while (base.getID() != 0){
+    _Link base = *this;
+    do{
         base = *base.getParent();
         idx.push_back(base);
+    }while(base.getID() != 0);
+    cout << "Route:\n";
+    for (int i = 0; i < idx.size(); i++){
+        cout << idx[i].getID() << "-->";
     }
+    cout << endl;
     MatrixXd jacobian =  MatrixXd::Zero(6,idx.size());
     Vector3d target = base.getPose();
-    for(int n = idx.size() -1; n >= 0; n--){
+    for(int n = idx.size() -2; n >= 0; n--){
         _Link mom = *(idx[n].getParent());
         Vector3d a = mom.getRot() * idx[n].a_;
         jacobian.block<3,1>(0,n) = a.cross(target - idx[n].getPose());
@@ -103,3 +113,61 @@ MatrixXd _Link::updateJacobian(){
 _Link::~_Link(){
 
 }
+
+/*
+int main(){
+    Vector3d a[12];
+	Vector3d b[12];
+	// Defining Joint Axis
+	a[0] << 0.0, 0.0, 1.0;
+	a[1] << 1.0, 0.0, 0.0;
+	a[2] << 0.0, 1.0, 0.0;
+	a[3] << 0.0, 1.0, 0.0;
+	a[4] << 0.0, 1.0, 0.0;
+	a[5] << 1.0, 0.0, 0.0;
+	a[6] = a[0];
+	a[7] = a[1];
+	a[8] = a[2];
+	a[9] = a[3];
+	a[10] = a[4];
+	a[11] = a[5];
+	// Defining Joint Positions
+	b[0] << 0.0, -0.115, 0.0;
+	b[1] << 0.0, 0.0, 0.0;
+	b[2] = b[1];
+	b[3] << 0.0, 0.0, -0.36;
+	b[4] << 0.0, 0.0, -0.37;
+	b[5] = b[1];
+	b[6] = -b[0];
+	b[7] = b[1];
+	b[8] = b[2];
+	b[9] = b[3];
+	b[10] = b[4];
+	b[11] = b[5];
+
+
+	_Link pelvis(0, Vector3d::Ones(3), Vector3d::Ones(3), 3.0, Matrix3d::Identity(3,3));
+	Vector3d position(0.0, 0.0, 0.74);
+	pelvis.initPose(position, Matrix3d::Identity(3, 3));
+
+	_Link rHipY(1, a[0], b[0], 3.0, Matrix3d::Identity(3, 3), &pelvis);
+	_Link rHipR(2, a[1], b[1], 3.0, Matrix3d::Identity(3, 3), &rHipY);
+	_Link rHipP(3, a[2], b[2], 3.0, Matrix3d::Identity(3, 3), &rHipR);
+	_Link rKnee(4, a[3], b[3], 3.0, Matrix3d::Identity(3, 3), &rHipP);
+	_Link rAnkleP(5, a[4], b[4], 3.0, Matrix3d::Identity(3, 3), &rKnee);
+	_Link rAnkleR(6, a[5], b[5], 3.0, Matrix3d::Identity(3, 3), &rAnkleP);
+
+	_Link lHipY(7, a[6], b[6], 3.0, Matrix3d::Identity(3, 3), &pelvis);
+	_Link lHipR(8, a[7], b[7], 3.0, Matrix3d::Identity(3, 3), &lHipY);
+	_Link lHipP(9, a[8], b[8], 3.0, Matrix3d::Identity(3, 3), &lHipR);
+	_Link lKnee(10, a[9], b[9], 3.0, Matrix3d::Identity(3, 3), &lHipP);
+	_Link lAnkleP(11, a[10], b[10], 3.0, Matrix3d::Identity(3, 3), &lKnee);
+	_Link lAnkleR(12, a[11], b[11], 3.0, Matrix3d::Identity(3, 3), &lAnkleP);
+
+
+	cout << lAnkleR.FK() << endl << "---------\n";
+	cout << lAnkleP.updateJacobian() << endl;
+	system("pause");
+
+    return 0;
+}*/
