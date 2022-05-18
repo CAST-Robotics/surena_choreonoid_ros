fdf38dd3d3cbc744b5c565d052e8dee11feef4ec
amirhosein-vedadi <amirhsein.vedadi@gmail.com>
2022-05-18 11:38:47 +0430

    covariance predict step was added.

--- EKFEstimator.cpp
+++ EKFEstimator.cpp
@@ -1,16 +1,38 @@
 #include "EKFEstimator.h"
 
-EKFEstimator::EKFEstimator() {
+explicit EKFEstimator::EKFEstimator() {
     Matrix3d rotation_matrix = Matrix3d::Identity();
-    GBaseAtitude_ = Quaterniond(rotation_matrix);
+    GBaseRot_ = rotation_matrix;
+    GBaseQuat_ = Quaterniond(rotation_matrix);
+
     statesDim_ = GBasePos_.size() + GBaseVel_.size() + 4 + GRightFootPos_.size() + 
+                 GLeftFootPos_.size() + BAccBias_.size() + BGyroBias_.size();
+
+    statesErrDim_ = GBasePos_.size() + GBaseVel_.size() + 3 + GRightFootPos_.size() + 
                     GLeftFootPos_.size() + BAccBias_.size() + BGyroBias_.size();
-     
+
     rvqStatesDim_ = GBasePos_.size() + GBaseVel_.size() + 4;
-    
+
+    processNoiseDim_ = BAcc_.size() + BGyro_.size() + GRightFootPos_.size() + 
+                       GLeftFootPos_.size() + BAccBias_.size() + BGyroBias_.size();
+
     xPrior_ = MatrixXd::Zero(statesDim_, 1);
     xPosterior_ = MatrixXd::Zero(statesDim_, 1);
+    Lc_ = MatrixXd::Zero(statesErrDim_, processNoiseDim_);
+    Qc_ = MatrixXd::Zero(processNoiseDim_, processNoiseDim_);
+    Fc_ = MatrixXd::Zero(statesErrDim_, statesErrDim_);
+    Fk_ = MatrixXd::Zero(statesErrDim_, statesErrDim_);
+    Qk_ = MatrixXd::Zero(statesErrDim_, statesErrDim_);
 
+    // noise parameters initilization
+    wf_ = 0.00078;
+    ww_ = 0.000523;
+    wp_r_ = 0.001;
+    wp_l_ = 0.001;
+    wbf_ = 0.0001;
+    wbw_ = 0.000618;
+    updateQc();
+    
     dt_ = 0.005;
 }
 
@@ -20,10 +42,6 @@ EKFEstimator::~EKFEstimator() {
 
 void EKFEstimator::setSensorData(Vector3d acc, Vector3d gyro){
 
-}
-
-void EKFEstimator::predict() {
-    predictState();
 }
 
 void EKFEstimator::predictState(){
@@ -37,7 +55,7 @@ void EKFEstimator::predictState(){
     xPrior_.segment(0,GBasePos_.size()) = GBasePos_ + dt_ * GBaseVel_ + (pow(dt_, 2) / 2) * (GBaseRot_ * BAcc_bar + Gravity_);
     xPrior_.segment(GBasePos_.size(), GBaseVel_.size()) = GBaseVel_ + dt_ * (GBaseRot_ * BAcc_bar + Gravity_);
     // predict orientation
-    Quaterniond predicted_q = delta_omega * GBaseAtitude_;
+    Quaterniond predicted_q = delta_omega * GBaseQuat_;
 
     xPrior_(GBasePos_.size() + GBaseVel_.size()) = predicted_q.x();
     xPrior_(GBasePos_.size() + GBaseVel_.size()  + 1) = predicted_q.y();
@@ -48,12 +66,60 @@ void EKFEstimator::predictState(){
     xPrior_(rvqStatesDim_, statesDim_ - rvqStatesDim_) = xPosterior_(rvqStatesDim_, statesDim_ - rvqStatesDim_);
 }
 
+void EKFEstimator::updateLc(Matrix3d rot) {
+    Lc_.block(3, 0, 3, 3) = -rot.transpose();
+    Lc_.block(6, 3, 3, 3) = -Matrix3d::Identity();
+    Lc_.block(9, 6, 3, 3) = rot.transpose(); // right foot
+    Lc_.block(12, 9, 3, 3) = rot.transpose(); // left foot
+    Lc_.block(15, 12, 3, 3) = Matrix3d::Identity();
+    Lc_.block(18, 15, 3, 3) = Matrix3d::Identity();
+}
+
+void EKFEstimator::updateQc() {
+    Qc_.block(0, 0, 3, 3) = wf_ * Matrix3d::Identity();
+    Qc_.block(3, 3, 3, 3) = ww_ * Matrix3d::Identity();
+    Qc_.block(6, 6, 3, 3) = wp_r_ * Matrix3d::Identity();
+    Qc_.block(9, 9, 3, 3) = wp_l_ * Matrix3d::Identity();
+    Qc_.block(12, 12, 3, 3) = wbf_ * Matrix3d::Identity();
+    Qc_.block(15, 15, 3, 3) = wbw_ * Matrix3d::Identity();
+}
+
+Matrix3d EKFEstimator::skewSym(const Vector3d &vec){
+    Matrix3d skew;
+    skew << 0, -vec(2), vec(1),
+            vec(2), 0, -vec(0),
+            -vec(1), vec(0), 0;
+    return skew;
+}
+
+void EKFEstimator::updateFc(Matrix3d rot) {
+    Fc_.block(0, 3, 3, 3) = Matrix3d::Identity();
+    Fc_.block(3, 6, 3, 3) = rot.transpose() * skewSym(BAcc_);
+    Fc_.block(3, 15, 3, 3) = -rot.transpose();
+    Fc_.block(6, 6, 3, 3) = -skewSym(BGyro_);
+    Fc_.block(6, 18, 3, 3) = -Matrix3d::Identity();
+}
+
+void EKFEstimator::predictCov() {
+    this->updateFc(GBaseRot_);
+    this->updateLc(GBaseRot_);
+    this->updateQc();
+
+    Fk_ = MatrixXd::Identity(statesErrDim_, statesErrDim_) + Fc_ * dt_;
+    Qk_ = Fk_ * Lc_ * Qc_ * Lc_.transpose() * Fk_.transpose() * dt_;
+    P_ = Fk_ * P_ * Fk_.transpose() + Qk_;
+}
+
+void EKFEstimator::predict() {
+    this->predictState();
+    this->predictCov();
+}
 
 void EKFEstimator::update() {
 
 }
 
 void EKFEstimator::updateState() {
-    
+
 }
 
--- EKFEstimator.h
+++ EKFEstimator.h
@@ -21,9 +21,15 @@ class EKFEstimator {
 
         void predict();
         void predictState();
+        void updateLc(Matrix3d rot);
+        void updateQc();
+        void updateFc(Matrix3d rot);
+        void predictCov();
         
         void update();
         void updateState();
+
+        Matrix3d skewSym(const Vector3d &vec);
 
     
     private:
@@ -31,7 +37,7 @@ class EKFEstimator {
         Vector3d GRightFootPos_;
         Vector3d GBasePos_;
         Vector3d GBaseVel_;
-        Quaterniond GBaseAtitude_;
+        Quaterniond GBaseQuat_;
         Matrix3d GBaseRot_;
 
 
@@ -44,12 +50,29 @@ class EKFEstimator {
         VectorXd xPrior_;
         VectorXd xPosterior_;
 
+        MatrixXd P_;
+
+        MatrixXd Lc_;
+        MatrixXd Qc_;
+        MatrixXd Fc_;
+        MatrixXd Fk_;
+        MatrixXd Qk_;
+
+        double wf_;
+        double ww_;
+        double wp_r_;
+        double wp_l_;
+        double wbf_;
+        double wbw_;
+
         vector<double> Config_;
         vector<double> ConfigDot_;
 
         Vector3d Gravity_;
 
         int statesDim_;
+        int statesErrDim_;
+        int processNoiseDim_;
         int rvqStatesDim_;
         double dt_;
 
