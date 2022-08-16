5338a41c96e3d6dec28fe5a6762a7caccdce4118
amirhosein-vedadi <amirhsein.vedadi@gmail.com>
2022-08-17 01:41:36 +0430

    camera image publisher was added tor choreonoid

--- surena_simulation/src/CameraController.cpp
+++ surena_simulation/src/CameraController.cpp
@@ -1,5 +1,9 @@
 #include <cnoid/SimpleController>
 #include <cnoid/Camera>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
 
 using namespace cnoid;
 
@@ -9,14 +13,18 @@ class CameraController : public SimpleController
     double timeCounter;
     double timeStep;
     std::ostream* os;
+    SimpleControllerIO* io_;
+    image_transport::Publisher imagePublisher_;
+    ros::NodeHandle nh;
 
 public:
     virtual bool initialize(SimpleControllerIO* io) override
     {
         os = &io->os();
-
+        io_ = io;
         cameras << io->body()->devices();
-
+        image_transport::ImageTransport it(nh);
+        imagePublisher_ = it.advertise("/image_raw", 1);
         for(size_t i=0; i < cameras.size(); ++i){
             Device* camera = cameras[i];
             io->enableInput(camera);
@@ -31,13 +39,39 @@ public:
         return true;
     }
 
+    void updateVisionSensor(Camera* sensor)
+    {
+        sensor_msgs::Image vision;
+        vision.header.stamp.fromSec(io_->currentTime());
+        vision.header.frame_id = sensor->name();
+        vision.height = sensor->image().height();
+        vision.width = sensor->image().width();
+        
+        if (sensor->image().numComponents() == 3)
+            vision.encoding = sensor_msgs::image_encodings::RGB8;
+        else if (sensor->image().numComponents() == 1)
+            vision.encoding = sensor_msgs::image_encodings::MONO8;
+        else {
+            ROS_WARN("unsupported image component number: %i", sensor->image().numComponents());
+        }
+        
+        vision.is_bigendian = 0;
+        vision.step = sensor->image().width() * sensor->image().numComponents();
+        
+        vision.data.resize(vision.step * vision.height);
+        std::memcpy(&(vision.data[0]), &(sensor->image().pixels()[0]), vision.step * vision.height);
+        
+        imagePublisher_.publish(vision);
+    }
+
     virtual bool control() override
     {
         timeCounter += timeStep;
-        if(timeCounter >= 1.0){
+        if(timeCounter >= 0.05){
             for(size_t i=0; i < cameras.size(); ++i){
                 Camera* camera = cameras[i];
                 std::string filename = camera->name() + ".png";
+                updateVisionSensor(camera);
                 camera->constImage().save(filename);
                 *os << "The image of " << camera->name()
                     << " has been saved to \"" << filename << "\"." << std::endl;
